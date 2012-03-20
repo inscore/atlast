0114340fea24a33cada630e06d05a780e1d39173
Scot Inscore <scot@orbotix.com>
2012-03-20 10:50:59 -0600

    change url from constant

--- lib/atlast/fulfillment.rb
+++ lib/atlast/fulfillment.rb
@@ -5,14 +5,14 @@ module Atlast
     def initialize(key, env="not-production")
       @key = key
       if env == "production"
-        ROOT_URL = "https://api.atlastfulfillment.com"
+        @root_url = "https://api.atlastfulfillment.com"
       else
-        ROOT_URL = "http://staging.api.atlastinc.com"
+        @root_url = "http://staging.api.atlastinc.com"
       end
     end
 
     def products
-      products_xml = RestClient.get(ROOT_URL + "/products.aspx", params: {key: key})
+      products_xml = RestClient.get(@root_url+"/products.aspx", params: {key: key})
       Crack::XML.parse products_xml
     end
 
@@ -23,7 +23,7 @@ module Atlast
       params = {key: key}
       params[:sku] = sku if sku
 
-      inventory_xml = RestClient.get ROOT_URL + "/inventory.aspx", params: params
+      inventory_xml = RestClient.get @root_url + "/inventory.aspx", params: params
       Crack::XML.parse inventory_xml
     end
 
@@ -58,19 +58,19 @@ module Atlast
           end
         end
       end
-      response = RestClient.post(ROOT_URL + "/post_shipments.aspx", xml, content_type: :xml, accept: :xml)
+      response = RestClient.post(@root_url + "/post_shipments.aspx", xml, content_type: :xml, accept: :xml)
       Crack::XML.parse response
     end
 
     def cancel(order_id)
       params = {key: key, orderId: order_id}
-      response = RestClient.get ROOT_URL + "/cancel_shipment.aspx", params: params
+      response = RestClient.get @root_url + "/cancel_shipment.aspx", params: params
       Crack::XML.parse response
     end
 
     def shipment_status(order_id)
       params = {key: key, id: order_id}
-      response = RestClient.get ROOT_URL + "/shipments.aspx", params: params
+      response = RestClient.get @root_url + "/shipments.aspx", params: params
       Crack::XML.parse response
     end
   end
